ef574cc9ab0212c90f09e6c65259f244bb3509a3
sachin121103 <67367792+sachin121103@users.noreply.github.com>
2021-06-17 15:28:41 +0800

    Update Basic Calculator.py

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -1,6 +1,6 @@
 import math
 
-# Defining the functions
+# Define the functions
 def add(a,b):
     return a+b
 
@@ -34,9 +34,9 @@ def square_root(a):
     else:
         print("Please enter a positive number")
 
+      
+# print statements
 
-# Introduction to Calculator
-print("Hello! Welcome to my calculator!)
 print("Select your operation:")
 print("1. Addition")
 print("2. Subtraction")
@@ -46,53 +46,75 @@ print("5. Factorial")
 print("6. Square Root")
 print("7. Square of a Number")
 
-# Code to run for input
+# Calculations
 while True:
-    acceptable_input = [1, 2, 3, 4, 5, 6, 7]
-
-    choice = int(input("Enter a number between 1-7: "))
     
-    if choice in acceptable_input:
-        
-        if choice == 1:
-            num1 = float(input("Enter the first number: "))
-            num2 = float(input("Enter the second number: "))
-            total = add(num1, num2)
-            print(total)
-            
-        if choice == 2:
-            num1 = float(input("Enter the first number: "))
-            num2 = float(input("Enter the second number: "))
-            subtraction = subtract(num1, num2)
-            print(subtraction)
-                
-        if choice == 3:
-            num1 = float(input("Enter the first number: "))
-            num2 = float(input("Enter the second number: "))
-            multiplication = multiply(num1, num2)
-            print(multiplication)
-            
-        if choice == 4:
-            num1 = float(input("Enter the first number: "))
-            num2 = float(input("Enter the second number: "))
-            division = divide(num1, num2)
-            print(division)
-            
-        if choice == 5:
-            num1 = int(input("Enter a number: "))
-            factorial_number = factorial(num1)
-            print(factorial_number)
-        
-        if choice == 6:
-            num1 = float(input("Enter a number: "))
-            square_root_of_number = square_root(num1)
-            print(square_root_of_number)
-            
-        if choice == 7:
-            num1 = float(input("Enter a number: "))
-            square_of_number = square(num1)
-            print(square_of_number)
-            
-    else:
-        print("You have entered unacceptable input")
+    try: 
+        acceptable_input = [1, 2, 3, 4, 5, 6, 7]
 
+        choice = int(input("Enter a number between 1-7: "))
+
+        if choice in acceptable_input:
+
+            if choice == 1:
+                try:
+                    num1 = float(input("Enter the first number: "))
+                    num2 = float(input("Enter the second number: "))
+                    total = add(num1, num2)
+                    print(total)
+                except ValueError:
+                    print("Please enter numbers")
+
+            if choice == 2:
+                try: 
+                    num1 = float(input("Enter the first number: "))
+                    num2 = float(input("Enter the second number: "))
+                    subtraction = subtract(num1, num2)
+                    print(subtraction)
+                except ValueError:
+                    print("Please enter numbers")
+
+            if choice == 3:
+                try:
+                    num1 = float(input("Enter the first number: "))
+                    num2 = float(input("Enter the second number: "))
+                    multiplication = multiply(num1, num2)
+                    print(multiplication)
+                except ValueError:
+                    print("Please enter numbers")
+
+            if choice == 4:
+                try:
+                    num1 = float(input("Enter the first number: "))
+                    num2 = float(input("Enter the second number: "))
+                    division = divide(num1, num2)
+                    print(division)
+                except ValueError:
+                    print("Please enter numbers")
+                except ZeroDivisionError:
+                    print("We cannot divide by 0! That breaks the universe. Change the value of the second number")
+            if choice == 5:
+                try:
+                    num1 = int(input("Enter an integer: "))
+                    factorialisation = factorial(num1)
+                    print(factorialisation)
+                except ValueError:
+                    print("Please enter a whole number")
+            if choice == 6:
+                try:
+                    num1 = float(input("Enter a number: "))
+                    square_root_of_number = square_root(num1)
+                    print(square_root_of_number)
+                except ValueError:
+                    print("Please enter numbers")
+            if choice == 7:
+                try:
+                    num1 = float(input("Enter a number: "))
+                    square_of_number = square(num1)
+                    print(square_of_number)
+                except ValueError:
+                    print("Please enter numbers")
+        else:
+            print("You have entered unacceptable input. Enter a number in the range")
+    except ValueError:
+        print("Please enter a whole number")
